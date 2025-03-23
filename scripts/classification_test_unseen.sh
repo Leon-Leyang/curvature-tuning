3e670a71c6bac03c327856d95fe576a82c4b9eda
leyang <leyang_hu@brown.edu>
2025-03-23 15:35:09 -0400

    Add script for experiments with test set unseen

---
+++ scripts/classification_test_unseen.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --time=144:00:00
+#SBATCH -N 1
+#SBATCH -p gpu --gres=gpu:1
+#SBATCH --mem=48G
+#SBATCH --cpus-per-task=8
+#SBATCH -J classification_test_unseen
+#SBATCH -o classification_test_unseen.log
+#SBATCH -e classification_test_unseen.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=leyang_hu@brown.edu
+
+# Load the necessary module
+module load miniconda3/23.11.0s
+
+eval "$(conda shell.bash hook)"
+
+# Activate the conda environment
+conda activate spline
+
+export PYTHONPATH=./
+
+models=(resnet18 resnet50 swin_t)
+pretrained_datasets=(imagenet)
+transfer_datasets=(arabic_characters beans fgvc_aircraft)
+train_percentages=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
+
+for model in "${models[@]}"
+do
+  for train_percentage in "${train_percentages[@]}"
+  do
+      for seed in 42 43 44
+      do
+          python -u classification.py --model $model --seed $seed --pretrained_ds ${pretrained_datasets[@]} --transfer_ds ${transfer_datasets[@]} --train_percentage $train_percentage
+      done
+  done
+done
