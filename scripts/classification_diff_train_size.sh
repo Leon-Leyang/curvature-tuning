c116a9239b2a799e523b6b08be44def01e169f51
leyang <leyang_hu@brown.edu>
2025-03-30 22:37:50 -0400

    Update scripts

--- scripts/classification_diff_train_size.sh
+++ scripts/classification_diff_train_size.sh
@@ -23,7 +23,7 @@ export PYTHONPATH=./
 
 models=(resnet18 resnet50 resnet152)
 pretrained_datasets=(imagenet)
-transfer_datasets=(arabic_characters arabic_digits beans cub200 dtd fashion_mnist fgvc_aircraft food101 med_mnist/dermamnist med_mnist/octmnist med_mnist/pathmnist)
+transfer_datasets=(arabic_characters arabic_digits beans dtd fashion_mnist fgvc_aircraft food101)
 train_percentages=(0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9)
 
 for model in "${models[@]}"
