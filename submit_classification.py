ee9d66c1a29527aa3c89b6b12eb8127fee170aea
leyang <leyang_hu@brown.edu>
2025-05-04 00:31:16 -0400

    Add submitit

---
+++ submit_classification.py
@@ -0,0 +1,74 @@
+import submitit
+import os
+from pathlib import Path
+
+
+def main(kwargs, job_dir):
+    model = kwargs['model']
+    pretrained_ds = kwargs['pretrained_ds']
+    transfer_ds = kwargs['transfer_ds']
+    seed = kwargs['seed']
+
+    # Set up the executor folder to include the job ID placeholder
+    executor = submitit.AutoExecutor(folder=job_dir / "%j")
+
+    # Define SLURM parameters here, adjust based on your cluster's specifics
+    executor.update_parameters(
+        mem_gb=48,                # Memory allocation
+        slurm_ntasks_per_node=1,  # Number of tasks per node
+        cpus_per_task=6,          # Number of CPUs per task
+        nodes=1,                  # Number of nodes
+        timeout_min=240,         # Maximum duration in minutes
+        slurm_partition="gpu", # Partition name
+        slurm_job_name=f"{pretrained_ds}_to_{transfer_ds}_{model}_seed{seed}",  # Job name
+        slurm_mail_type="ALL",    # Email settings
+        slurm_mail_user="leyang_hu@brown.edu",  # Email address
+    )
+
+    command = (
+        "module load miniconda3/23.11.0s && "
+        "conda activate spline && "
+        f"python -u classification.py --model {model} --pretrained_ds {pretrained_ds} --transfer_ds {transfer_ds} --seed {seed}"
+    )
+
+    # Submit the job
+    job = executor.submit(os.system, command)
+    print(f"Job submitted for model {model} on dataset {pretrained_ds}_to_{transfer_ds} with seed {seed} with job ID {job.job_id}")
+
+
+if __name__ == "__main__":
+    # Create the directory where logs and results will be saved
+    job_dir = Path("./submitit")
+    job_dir.mkdir(parents=True, exist_ok=True)
+
+    # List of models
+    model_list = [
+        "resnet18",
+        "resnet50",
+        "resnet152",
+    ]
+
+    # List of datasets to transfer to
+    dataset_list = [
+        "arabic-characters",
+        "arabic-digits",
+        "beans",
+        "cub200",
+        "dtd",
+        "fashion-mnist",
+        "fgvc-aircraft",
+        "flowers102",
+        "food101",
+        "medmnist/dermamnist",
+        "medmnist/octmnist",
+        "medmnist/pathmnist",
+    ]
+
+    seed_list = [42, 43, 44]
+
+    for seed in seed_list:
+        for model in model_list:
+            for transfer_ds in dataset_list:
+                filepath = f"./logs/classification_imagenet_to_{transfer_ds}_{model}_seed{seed}.log"
+                if not os.path.exists(filepath):
+                    main({'model': model, 'pretrained_ds': 'imagenet', 'transfer_ds': transfer_ds, 'seed': seed}, job_dir)
